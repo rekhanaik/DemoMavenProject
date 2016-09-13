42a939abee173700e975c5f11e2014595fd51ee2
rekhanaik <rekhalearn@gmail.com>
2016-09-13 07:26:30 +0100

    First Commit

---
+++ src/main/java/JenkinsEBook/DemoMavenProject/WebSite.java
@@ -0,0 +1,44 @@
+package JenkinsEBook.DemoMavenProject;
+
+public class WebSite {
+
+private Boolean isStarted = false;
+private String language;
+private Boolean isPublic = false;
+
+public WebSite(String language, Boolean isPublic)
+{
+     this.language = language;
+     this.isPublic = isPublic;
+}
+
+public void start() {
+   this.isStarted = true;
+}
+
+public void stop() {
+  this.isStarted = false;
+}
+
+public void makePublic() {
+this.isPublic = true;
+
+}
+
+public void makePrivate() {
+this.isPublic = false;
+}
+
+public String language() {
+    return this.language;
+}
+
+public Boolean isStarted() {
+  return this.isStarted;
+
+}
+
+public Boolean isPublic() {
+   return this.isPublic;
+}
+}
---
+++ src/test/java/JenkinsEBook/DemoMavenProject/WebSiteTests.java
@@ -0,0 +1,41 @@
+package JenkinsEBook.DemoMavenProject;
+
+import org.testng.annotations.Test;
+
+public class WebSiteTests {
+
+	   @Test
+	    public void testSite1()
+	    {
+		WebSite site = new WebSite("english",false);
+	    }
+
+	   @Test
+	    public void testSite2()
+	    {
+
+		WebSite site = new WebSite("english",false);
+	                site.start();
+		
+	    }
+
+
+	  @Test
+	  public void testSite3()
+	    {
+
+		WebSite site = new WebSite("english",false);
+	                site.start();
+	                site.stop();
+		}
+
+	  @Test
+	   public void testSite4()
+	    {
+
+		WebSite site = new WebSite("english",false);
+	                site.start();
+		site.makePublic();
+		}
+	}
+
